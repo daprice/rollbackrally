febaf7bd9e1dc69240d126f9eea62ffc717686dc
Dale Price <daprice@mac.com>
2022-12-03 05:21:55 +0000

    basic odometer drawing test

---
+++ Source/Odometer.lua
@@ -0,0 +1,47 @@
+local gfx <const> = playdate.graphics
+
+local twoPi <const> = math.pi * 2
+
+local odometerNumbers = gfx.imagetable.new("/assets/fonts/Roobert-24-Medium-Numerals-table-36-36")
+local numberWidth, numberHeight <const> = odometerNumbers:getImage(1):getSize()
+local odometerStrip = gfx.image.new(numberWidth, numberHeight * 10, gfx.kColorBlack)
+gfx.pushContext(odometerStrip)
+gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
+for n = 1, 10 do
+	odometerNumbers:drawImage(n, 0, (n - 1) * numberHeight)
+end
+gfx.popContext()
+
+class("Odometer", {
+	miles = 0,
+}).extends(gfx.sprite)
+
+function Odometer:init()
+	Odometer.super.init(self)
+end
+
+function Odometer.drawValue(value)
+	local placeValues <const> = {
+		(value % 1000000) / 100000,
+		(value % 100000)  / 10000,
+		(value % 10000)   / 1000,
+		(value % 1000)    / 100,
+		(value % 100)     / 10,
+		(value % 10),
+	}
+	
+	for d = 1, 6 do
+		Odometer.drawDigit(placeValues[d], (d-1) * numberWidth, 0)
+	end
+end
+
+function Odometer.drawDigit(digitValue, x, y)
+	local position <const> = numberHeight * (digitValue - 0.5 + (math.sin((digitValue) * twoPi) / 7))
+	odometerStrip:draw(x, y, nil, 0, position, numberWidth, numberHeight)
+	if position + numberHeight > numberHeight * 10 then
+		odometerStrip:draw(x, y, nil, 0, position - (numberHeight * 10), numberWidth, numberHeight)
+	end
+	if position < 0 then
+		odometerStrip:draw(x, y, nil, 0, position + (numberHeight * 10), numberWidth, numberHeight)
+	end
+end
--- Source/main.lua
+++ Source/main.lua
@@ -5,13 +5,23 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/animator"
 import "CoreLibs/easing"
+import "Odometer"
 
 local gfx <const> = playdate.graphics
 
 playdate.display.setRefreshRate(30)
 
+value = 0
+
+
 function playdate.update()
 	gfx.sprite.update()
+	
+	value += playdate.getCrankChange() / 100
+	print(value)
+	
+	Odometer.drawValue(value)
+	
 	if debugMode then
 		playdate.drawFPS(0, 228)
 	end
