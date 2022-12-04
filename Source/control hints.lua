80dc746c177de779c7fb9e1dcb98b065758b37fc
Dale Price <daprice@mac.com>
2022-12-04 06:40:26 +0000

    use bold font for stuff

--- Source/control hints.lua
+++ Source/control hints.lua
@@ -8,13 +8,14 @@ local padding <const> = 4
 class("ControlHint").extends(gfx.sprite)
 
 function ControlHint:init(text)
+	gfx.setFont(hintFont)
 	local textHeight <const> = hintFont:getHeight()
-	local textWidth <const> = hintFont:getTextWidth(text)
+	local textWidth, _ <const> = gfx.getTextSize(text)
 	local img <const> = gfx.image.new(textWidth + padding*2, textHeight + padding*2, gfx.kColorClear)
 	gfx.pushContext(img)
 	pillNineSlice:drawInRect(0, 0, img:getSize())
 	gfx.setImageDrawMode(gfx.kDrawModeNXOR)
-	hintFont:drawText(text, 4, 4)
+	gfx.drawText(text, 4, 4)
 	gfx.popContext()
 	ControlHint.super.init(self, img)
 	self:setZIndex(100)
@@ -23,14 +24,14 @@ end
 
 ControlHint.hints = {}
 
-ControlHint.hints.crank = ControlHint("🎣 Wind")
+ControlHint.hints.crank = ControlHint("🎣 *Wind*")
 ControlHint.hints.crank:setCenter(0, 0)
 
-ControlHint.hints.aButton = ControlHint("Ⓐ Sell")
+ControlHint.hints.aButton = ControlHint("Ⓐ *Sell*")
 ControlHint.hints.aButton:setCenter(0, 0)
 
-ControlHint.hints.nextCar = ControlHint("Ⓐ Next Vehicle")
+ControlHint.hints.nextCar = ControlHint("Ⓐ *Next Vehicle*")
 ControlHint.hints.nextCar:setCenter(0, 0)
 
-ControlHint.hints.broken = ControlHint("Uh oh, you broke it")
+ControlHint.hints.broken = ControlHint("*Uh oh, you broke it*")
 ControlHint.hints.broken:moveTo(200, 120)
